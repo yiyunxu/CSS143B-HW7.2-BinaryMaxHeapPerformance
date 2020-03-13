5e401a0f74409b11a6f77379166347f9422228bc
Elaine Xu <yeeyun_@hotmail.com>
2020-03-12 17:03:02 -0700

    initial commit

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -1,41 +1,121 @@
 public class MaxHeap implements Heap {
-    int capacity;
     int size;
-
+    int capacity;
     Integer[] data;
 
-    public MaxHeap(int size) {
-        this.size = size;
-        data = new Integer[size];
+    public MaxHeap(int capacity) {
+        data = new Integer[capacity];
+        this.capacity = capacity;
+        size = 0;
     }
 
     // build a heap based on data
     // to be implemented in O(nlogn)
     public void MaxHeapLogN(Integer[] data) {
         // homework
+        for (int i = 0; i < data.length; i++){
+            add(data[i]);
+        }
     }
 
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
         // homework
+        this.data = data.clone();
+        size = data.length;
+        int idx = (data.length-1-1)/2;
+        for (int i = idx; i >= 0; i--){
+            heapifyDown(i);
+        }
     }
 
     // add an item to the heap
     public boolean add(Integer item) {
         // homework
-        return false;
+        if (size == capacity){
+            return false;
+        }
+        data[size] = item;
+        heapifyUp(size);
+        size++;
+        return true;
+    }
+
+    //helper
+    private void heapifyUp(int n){
+        if (n != 0 && data[n] > data[(n-1)/2]) {
+            swap(n, (n - 1) / 2);
+            heapifyUp((n-1)/2);
+        }
+    }
+
+    //helper
+    private void swap(int i, int j){
+        int temp = data[i];
+        data[i] = data[j];
+        data[j] = temp;
     }
 
     // return the max item in the heap
     public Integer get() {
         // homework
-        return null;
+        return data[0];
     }
 
     // remove the root item
     public Integer pop() {
         // homework
-        return null;
+        int result = data[0];
+        data[0] = data[size-1];
+        size--;
+        heapifyDown(0);
+        return result;
+    }
+
+    //helper
+    private void heapifyDown(int n){
+        int biggest = n;
+        int left = n*2+1; // child of n
+        int right = n*2+2; // child of n
+        if (left < size && data[left] > data[biggest]) {
+            biggest = left;
+        }
+        if (right < size && data[right] > data[biggest]) {
+            biggest = right;
+        }
+        if (biggest!= n){
+            swap(biggest, n);
+            heapifyDown(biggest);
+        }
+    }
+
+    public boolean equal(int[] otherData){
+        if (size != otherData.length)
+            return false;
+        for (int i = 0; i < size; i++){
+            if (!data[i].equals(otherData[i]))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean equal(Integer[] otherData){
+        if (size != otherData.length)
+            return false;
+        for (int i = 0; i < size; i++){
+            if (!data[i].equals(otherData[i]))
+                return false;
+        }
+        return true;
+    }
+
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < size; i++) {
+            str += data[i];
+
+        }
+        return str;
     }
 }
--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,5 +1,140 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.*;
 
 public class MaxHeapTest {
     // homework
+
+    @Test
+    public void testAdd() {
+        int[][] expected = {{2},
+                            {4, 2},
+                            {7, 2, 4},
+                            {7, 7, 2, 1}};
+        //add one element
+        int testSize = 1;
+        MaxHeap heap = new MaxHeap(testSize);
+        assertTrue(heap.add(2));
+        assertTrue(heap.equal(expected[0]));
+
+        // add two with no duplicates
+        testSize = 5;
+        heap = new MaxHeap(testSize);
+        assertTrue(heap.add(2));
+        assertTrue(heap.add(4));
+        assertTrue(heap.equal(expected[1]));
+
+        // add three with no duplicates
+        testSize = 5;
+        heap = new MaxHeap(testSize);
+        assertTrue(heap.add(2));
+        assertTrue(heap.add(4));
+        assertTrue(heap.add(7));
+        assertTrue(heap.equal(expected[2]));
+
+        // add four with duplicates
+        testSize = 5;
+        heap = new MaxHeap(testSize);
+        assertTrue(heap.add(2));
+        assertTrue(heap.add(1));
+        assertTrue(heap.add(7));
+        assertTrue(heap.add(7));
+        assertTrue(heap.equal(expected[3]));
+    }
+
+    @Test
+    public void testGet() {
+        Integer[][] input = {{2},            //one element
+                             {2, 4},         //two with no duplicates
+                             {5, 3},         //two with no duplicates
+                             {2, 4, 7},      //three with no duplicates
+                             {2, 1, 7, 7}};  //four with duplicates
+        Integer[] expected = {2, 4, 5, 7, 7};
+
+        for (int i=0; i< input.length; i++){
+            int testSize = input[i].length;
+            MaxHeap heap = new MaxHeap(testSize);
+
+            for (int j=0; j< input[i].length; j++){
+                assertTrue(heap.add(input[i][j]));
+            }
+            assertEquals(heap.get(), expected[i]);
+        }
+    }
+
+    @Test
+    public void testPop() {
+        Integer[][] input = {{2},            //one element
+                             {2, 4},         //two with no duplicates
+                             {5, 3},         //two with no duplicates
+                             {2, 4, 7},      //three with no duplicates
+                             {2, 1, 7, 7}};  //four with duplicates
+        Integer[] expectedPop = {2, 4, 5, 7, 7};
+        int[][] expectedData = {{},
+                                {2},
+                                {3},
+                                {4, 2},
+                                {7, 1, 2}};
+
+        for (int i=0; i< input.length; i++){
+            int testSize = input[i].length;
+            MaxHeap heap = new MaxHeap(testSize);
+
+            for (int j=0; j< input[i].length; j++){
+                assertTrue(heap.add(input[i][j]));
+            }
+            assertEquals(heap.pop(), expectedPop[i]);
+            assertTrue(heap.equal(expectedData[i]));
+        }
+    }
+
+    @Test
+    public void testMaxHeapLogN(){
+        Integer[][] input = {{2},            //one element
+                             {2, 4},         //two with no duplicates
+                             {5, 3},         //two with no duplicates
+                             {2, 4, 7},      //three with no duplicates
+                             {2, 1, 7, 7}};  //four with duplicates
+        int[][] expected = {{2},
+                            {4, 2},
+                            {5, 3},
+                            {7, 2, 4},
+                            {7, 7, 2, 1}};
+        for (int i = 0; i< input.length; i++){
+            int testSize = input[i].length;
+            MaxHeap heap = new MaxHeap(testSize);
+            heap.MaxHeapLogN(input[i]);
+            //System.out.println(heap);
+            assertTrue(heap.equal(expected[i]));
+        }
+    }
+
+    @Test
+    public void testMaxHeapN(){
+        Integer[][] input = {{2},
+                {2, 4},
+                {5, 3},
+                {2, 4, 7},
+                {2, 1, 7, 7}};
+        int[][] expected = {{2},
+                {4, 2},
+                {5, 3},
+                {7, 4, 2},
+                {7, 2, 7, 1}};
+        for (int i = 0; i< input.length; i++){
+            int testSize = input[i].length;
+            MaxHeap heap = new MaxHeap(testSize);
+            heap.MaxHeapN(input[i]);
+            //System.out.println(heap);
+            assertTrue(heap.equal(expected[i]));
+        }
+    }
+
+
+
 }
